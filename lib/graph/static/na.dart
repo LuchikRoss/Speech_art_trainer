ee3fa17e1c317cf5f78467e542f79ec3252e1cdd
Rostyslav <lucianrossweb@gmail.com>
2020-11-25 19:16:28 +0200

    Revert "Revert "init""
    This reverts commit df25004479973c47f80aa4a4dd2cfb2d9bc56f61.

---
+++ lib/graph/static/na.dart
@@ -0,0 +1,17 @@
+naLoad() {
+
+   var na=[], na1=[];
+   
+   na1.add([100 , 93]);
+   na1.add([100 , 86]);
+   na1.add([98 , 79]);
+   na1.add([92 , 74]);
+   na1.add([85 , 70]);
+   na1.add([80 , 66]);
+   na1.add([77 , 60]);
+   na1.add([78 , 53]);
+
+   na.add(na1);
+
+   return na;
+}
